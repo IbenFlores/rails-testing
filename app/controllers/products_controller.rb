dd4a62090ecc86a1742a681290ab3b5fcd2d462c
Iben Polanco <iben.fpolanco@gmail.com>
2022-09-02 14:52:41 -0500

    pls give me my job back

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,22 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(params[:product])
+    @product.user = current_user
+    if @product.save
+      flash[:success] = "Object successfully created"
+      redirect_to root_path
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+
 end
